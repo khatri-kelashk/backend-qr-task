a8cad8c0186893c7d604eb175aafa4d8fab5e68b
Kelash Kumar Khatri <khatri.kelash@yahoo.com>
2025-05-12 23:44:08 +0500

    Feat: Login Route + Controller added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 import { User } from '../models/userSchema.js';
 import { catchAsyncError } from '../middleware/catchAsyncError.js';
+import { sendToken } from "../utils/jwtToken.js";
 import ErrorHandler from '../middleware/error.js';
 
 export const register = catchAsyncError( async (req, res, next) => {
@@ -12,11 +13,23 @@ export const register = catchAsyncError( async (req, res, next) => {
        return next(new ErrorHandler("Email already exists!")); 
     }
     const user = await User.create({ name, email, phone, role , password});
-    res.status(200).json({
-        success: true, message: 'Registration successful', user
-    });
-})
+    
+    sendToken(user, 200, res, "User registered successfully!");
+});
 
-export const login = () => {
+export const login = catchAsyncError( async (req, res, next) => {
+    const { email, password } = req.body;    
+    if ( !email || !password) {
+        return next(new ErrorHandler("Please provide, Email and Password!", 400));
+    }
+    const UserExist = await User.findOne({ email: email}).select("+password");
+    if (!UserExist) {
+       return next(new ErrorHandler("Invalid Email or Password!")); 
+    }
+    const isPasswordMatched = await User.comparePassword(password);
+    if (!isPasswordMatched) {
+        new ErrorHandler("Invalid Email or Password", 400);
+    }
+    sendToken(UserExist, 200, res, "User logged in successfully!");
 
-}
+});
