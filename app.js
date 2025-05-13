0537647eb17abed47e647cf73577d6a2ae6aa923
Kelash Kumar Khatri <khatri.kelash@yahoo.com>
2025-05-14 02:02:21 +0500

    Fix: Environment sample file added + Test API added

--- app.js
+++ app.js
@@ -2,19 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
 import userRouter from "./routes/userRouter.js";
 import { dbConnection } from "./database/dbConnection.js";
 import { errorMiddleware } from "./middleware/error.js";
 
 
 const app = express();
-dotenv.config({path: "./config/config.env"});
 
+dotenv.config({path: "./config/config.env"});
+app.use(helmet());
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
+    // credentials: true,
   })
 );
 app.use(cookieParser());
@@ -22,6 +24,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use("/api/user", userRouter);
+app.use("/api/test",(req, res)=>{
+    res.json({
+        success: true,
+        message: "API is running!",
+    });
+});
 
 dbConnection();
 
